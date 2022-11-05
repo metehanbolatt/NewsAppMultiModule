7580a2d0ed8587d7b450eb8aeb90a12ddb1a840c
Metehan <metehan.bolat.ie@gmail.com>
2022-11-06 01:31:28 +0300

    * Splash Screen was completed.

--- app/src/main/java/com/metehanbolat/newsappmultimodule/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/newsappmultimodule/MainActivity.kt
@@ -2,10 +2,18 @@ package com.metehanbolat.newsappmultimodule
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.metehanbolat.newsappmultimodule.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        window.statusBarColor = ContextCompat.getColor(this, R.color.white)
+        setContentView(view)
     }
 }
